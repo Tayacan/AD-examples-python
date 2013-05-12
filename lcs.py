fd0d8582ba4c86b4a663e9f68f60adde8ab5ada2
= <=>
2013-05-12 18:53:15 +0200

    Demonstration of the dramatic effect of memoize

--- lcs.py
+++ lcs.py
@@ -1,3 +1,8 @@
+from fib import memoize
+import random
+
+# comment out the @memoize decorator to see the difference it makes.
+@memoize
 def lcs(a,b):
     if (len(a) == 0) or (len(b) == 0):
         return []
@@ -9,3 +14,6 @@ def lcs(a,b):
     lcs_b = lcs(a[:-1],b)
 
     return max(lcs_a,lcs_b,key=len)
+
+# lcs of two randomly generated strings of 0's and 1's, each of length 100.
+print ''.join(lcs(''.join([random.choice('10') for x in range(100)]),''.join([random.choice('10') for x in range(100)])))
