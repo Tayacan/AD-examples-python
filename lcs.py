250b209b267c7d1426f5e1d7275eee5e8d4745f2
Maya Saietz <mayasaietz@gmail.com>
2013-05-13 02:21:19 +0200

    Testing a hypothesis - it seems to be true, but can we prove it?

--- lcs.py
+++ lcs.py
@@ -16,4 +16,14 @@ def lcs(a,b):
     return max(lcs_a,lcs_b,key=len)
 
 # lcs of two randomly generated strings of 0's and 1's, each of length 100.
-print ''.join(lcs(''.join([random.choice('10') for x in range(100)]),''.join([random.choice('10') for x in range(100)])))
+#print ''.join(lcs(''.join([random.choice('10') for x in range(100)]),''.join([random.choice('10') for x in range(100)])))
+
+# Test of the LPS(S) == LCS(S,S') theory
+c = 0
+for i in range(10000):
+    s = ''.join([random.choice('abcdefghijklmnopqrstuvxyz') for x in range(random.randrange(10,20))])
+    p = lcs(s,s[::-1])
+    if p != p[::-1]:
+        c += 1
+
+print c
