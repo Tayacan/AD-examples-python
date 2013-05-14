5d7478e0f4b0447fae480952cc08e26089ec6c8a
Maya Saietz <mayasaietz@gmail.com>
2013-05-14 16:03:40 +0200

    Improvals to the hash table!
    Now supports deletion, and figuring out if a key is even in the table.

--- hashing.py
+++ hashing.py
@@ -5,19 +5,24 @@ class Item:
         self.key = key
         self.value = value
 
+    def __repr__(self):
+        return str(self.value)
+
 class HashTable:
     def __init__(self,size,p=1009):
         self.size = size
         self._lst = [[] for x in range(self.size)]
         self.h = self._getHashFunction(p)
 
-    def __getitem__(self,k):
-        return self.get(k)
+    def __getitem__(self,key):
+        hkey = self.h(key)
+        for elem in self._lst[hkey]:
+            if elem.key == key:
+                return elem.value
+
+        raise IndexError
 
     def __setitem__(self,key,value):
-        self.insert(key,value)
-
-    def insert(self,key,value):
         item = Item(key,value)
         hkey = self.h(key)
         for i in range(len(self._lst[hkey])):
@@ -27,13 +32,29 @@ class HashTable:
 
         self._lst[hkey].append(item)
 
-    def get(self,key):
-        hkey = self.h(key)
-        for elem in self._lst[hkey]:
-            if elem.key == key:
-                return elem.value
+    def __repr__(self):
+        return str(self._lst)
 
-        return None
+    def __len__(self):
+        return sum(map(len,self._lst))
+
+    def __contains__(self,key):
+        hkey = self.h(key)
+        for item in self._lst[hkey]:
+            if item.key == key:
+                return True
+
+        return False
+
+    def __delitem__(self,key):
+        if not key in self:
+            raise IndexError
+
+        hkey = self.h(key)
+        for i in range(len(self._lst[hkey])):
+            if self._lst[hkey][i].key == key:
+                del self._lst[hkey][i]
+                return
 
     def _getHashFunction(self,p):
         a = random.randrange(1,p)
